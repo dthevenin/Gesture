66f37c5378aea3aff5b4df543462965ad185bcad
David Thevenin <david.thevenin@gmail.com>
2013-06-04 10:29:41 +0900

    Update libs

--- build/vs_pointer_standalone.js
+++ build/vs_pointer_standalone.js
@@ -312,7 +312,7 @@ function msPointerCancelHandler (event, listener)
 
 /*************************************************************/
 
-var pointerStartHandler, pointerMoveHandler, pointerEndHandle, pointerCancelHandler;
+var pointerStartHandler, pointerMoveHandler, pointerEndHandler, pointerCancelHandler;
 
 if (EVENT_SUPPORT_TOUCH)
 {
