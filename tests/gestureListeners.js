598fe487fabb343c044fe284a8d0a0aa46172a3e
David Thevenin <david.thevenin@gmail.com>
2018-01-02 18:47:49 +0900

    fix test

--- tests/gestureListeners.js
+++ tests/gestureListeners.js
@@ -6,15 +6,15 @@ function handleEvent (e)
 {
   switch (e.type)
   {
-    case vs.GESTURE_START:
+    case vs_gesture.GESTURE_START:
       this.gestureStart (e);
       break;
       
-    case vs.GESTURE_CHANGE:
+    case vs_gesture.GESTURE_CHANGE:
       this.gestureChange (e);
       break;
       
-    case vs.GESTURE_END:
+    case vs_gesture.GESTURE_END:
       this.gestureEnd (e);
       break;
    }
@@ -23,8 +23,8 @@ function handleEvent (e)
 
 function gestureStart (e)
 {
-  vs.addPointerListener (document, vs.GESTURE_CHANGE, this);
-  vs.addPointerListener (document, vs.GESTURE_END, this);
+  vs_gesture.addPointerListener (document, vs_gesture.GESTURE_CHANGE, this);
+  vs_gesture.addPointerListener (document, vs_gesture.GESTURE_END, this);
   this.vsSetNewTransformOrigin (e.centroid);
 
   //update_debug (e.targetPointerList, e.changedPointerList, false, e.rotation);
@@ -42,8 +42,8 @@ function gestureChange (e)
 function gestureEnd (e)
 {
   //update_debug (null, null, true);
-  vs.removePointerListener (document, vs.GESTURE_END, this);
-  vs.removePointerListener (document, vs.GESTURE_CHANGE, this);
+  vs_gesture.removePointerListener (document, vs_gesture.GESTURE_END, this);
+  vs_gesture.removePointerListener (document, vs_gesture.GESTURE_CHANGE, this);
 };
   
 
